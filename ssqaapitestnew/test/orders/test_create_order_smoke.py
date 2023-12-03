1ee722d3b4ca09f5754acd0c21f67fb9b73fc378
aswanym <aswanymahendran222@gmail.com>
2023-12-03 13:13:20 +0530

    tcid48 order completed

--- ssqaapitestnew/test/orders/test_create_order_smoke.py
+++ ssqaapitestnew/test/orders/test_create_order_smoke.py
@@ -1,10 +1,11 @@
 import pdb
-
+from ssqaapitestnew.src.dao.orders_dao import OrdersDAO
 from ssqaapitestnew.src.dao.products_dao import ProductDAO
 from ssqaapitestnew.src.helpers.order_helper import OrderHelper
 
 import pytest
 
+@pytest.mark.orders
 @pytest.mark.tcid48
 def test_create_order_guest_user():
 
@@ -24,7 +25,6 @@ def test_create_order_guest_user():
     }
     order_helper = OrderHelper()
     order_json = order_helper.create_order(additional_args=info)
-    pdb.set_trace()
 
     # verify response
     assert order_json, f"Create order response is empty."
@@ -36,4 +36,18 @@ def test_create_order_guest_user():
 
 
     # verify db
-    pass
+    order_dao = OrdersDAO()
+    order_id = order_json['id']
+    line_info = order_dao.get_order_lines_by_order_id(order_id)
+    assert line_info, f"Create order, line item not created in db. order id : {order_id}"
+
+    line_items = [info for info in line_info if info['order_item_type'] == 'line_item']
+    assert len(line_items) == 1, f"Expected one line item but found {len(line_items)}. order id {order_id}"
+
+    line_item_id = line_items[0]['order_item_id']
+    item_details = order_dao.get_order_items_details(line_item_id)
+    db_product_id = item_details['_product_id']
+
+    assert str(db_product_id) == str(product_id), f"Create order order 'product id ' is doesn't match with API. " \
+                                        f"API product id {product_id}, db product id {db_product_id}"
+
