b597527a5032237b2cedbfe76aab1fb3390fddc7
aswanym <aswanymahendran222@gmail.com>
2023-12-03 14:51:07 +0530

    set fxtures to tcid48, tcid49

--- ssqaapitestnew/test/orders/test_create_order_smoke.py
+++ ssqaapitestnew/test/orders/test_create_order_smoke.py
@@ -5,16 +5,30 @@ from ssqaapitestnew.src.helpers.order_helper import OrderHelper
 from ssqaapitestnew.src.helpers.customers_helper import CustomerHelper
 import pytest
 
-@pytest.mark.orders
-@pytest.mark.tcid48
-def test_create_order_guest_user():
+@pytest.fixture(scope='module')
+def my_order_smoke_setup():
 
     # get a product from db
     product_dao = ProductDAO()
     rand_product = product_dao.get_random_product(1)
     product_id = rand_product[0]['ID']
 
+    # create order helper
+    order_helper = OrderHelper()
+
+    info = {
+        "product_id": product_id,
+        "order_helper": order_helper,
+    }
+    return info
+
+
+@pytest.mark.orders
+@pytest.mark.tcid48
+def test_create_order_guest_user(my_order_smoke_setup):
+
     customer_id = 0
+    product_id = my_order_smoke_setup['product_id']
 
     # To update the order creation json give product from our db.
     info = {
@@ -26,23 +40,20 @@ def test_create_order_guest_user():
         ],
         "customer_id": customer_id
     }
-    order_helper = OrderHelper()
+    order_helper = my_order_smoke_setup['order_helper']
     order_json = order_helper.create_order(additional_args=info)
 
     product_list = [{'product_id': product_id}]
 
     # varify order created
-    order_helper.varify_order_created (order_json, customer_id, product_list)
+    order_helper.varify_order_created(order_json, customer_id, product_list)
 
 
 @pytest.mark.orders
 @pytest.mark.tcid49
-def test_create_paid_order_new_created_customer():
+def test_create_paid_order_new_created_customer(my_order_smoke_setup):
 
-    # get a product from db
-    product_dao = ProductDAO()
-    rand_product = product_dao.get_random_product(1)
-    product_id = rand_product[0]['ID']
+    product_id = my_order_smoke_setup['product_id']
 
     # create a customer
     cust_helper = CustomerHelper()
@@ -62,7 +73,7 @@ def test_create_paid_order_new_created_customer():
     }
 
     # create order
-    order_helper = OrderHelper()
+    order_helper = my_order_smoke_setup['order_helper']
     order_json = order_helper.create_order(additional_args=info)
 
     product_list = [{'product_id': product_id}]
